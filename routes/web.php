c89c8433cd6d056735144b17f4f873044497765e
Douvon Kossi <douvonk78@gmail.com>
2026-09-09 12:14:53 +0200

    Fix route accueil

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('accueil');
-});
+})->name('accueil');
 
-Route::get('/contact', function () { return view('contact'); })->name('contact');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
